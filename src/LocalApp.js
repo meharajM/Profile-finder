63e3d353cd0951c1fe8fdd9970d3a30219bfd9ae
meharaj <meharaj.m@snapwiz.com>
2017-02-23 18:29:33 +0530

    handling ajax using XMLHttpRequest in ReactJS app

--- src/LocalApp.js
+++ src/LocalApp.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import 'bootstrap/less/bootstrap.less';
+
 
 //import App from './App';
 import './index.css';
 /*
+sudo npm star to run server
+
 Components : 
   App
     Search
@@ -17,6 +25,8 @@ Components :
       Candidate
     
 */
+//the fallowing code would make the app to work without the router. i.e. search and filter will work without sending any requests to the api search and filetr in local data
+
 function Experience(props){
 	const experience = props.experience;
 	let list = experience.map((exp)=>{return exp.company});
@@ -28,12 +38,14 @@ function Candidate(props){
 	const candidate = props.candidate;
 	return(
 			<div>
-				<img src={candidate.profile_picture} />
-				<div>{candidate.first_name+" "+candidate.last_name}</div>
-				<div><span>{candidate.current_role}</span> at {candidate.current_company}</div>
-				<div>({candidate.total_experience})</div>
-				<div>{candidate.current_location}</div>
-				<div>Worked at:<span><Experience experience={candidate.experience}/></span></div>
+				<div className='canImg'><img src={candidate.profile_picture} /></div>
+				<div className='canDetails'>
+					<div>{candidate.first_name+" "+candidate.last_name}</div>
+					<div><span>{candidate.current_role}</span> at {candidate.current_company}</div>
+					<div>({candidate.total_experience})</div>
+					<div>{candidate.current_location}</div>
+					<div>Worked at:<span><Experience experience={candidate.experience}/></span></div>
+				</div>
 			</div>
 		);
 }
@@ -63,9 +75,9 @@ function SearchName(props){
 	return(<input name="searchName" type="text" placeholder="search name" onChange={props.whenChanged}/>);
 }
 function Header(props){
-	let count = props.filterCompany !== 'all' ? 1 : 0;
-	count = props.filterLocation !== 'all' ? count + 1 : count;
-	count = props.searchName !== '' ? count + 1 : count;  
+	let count = props.filterCompany != 'all' ? 1 : 0;
+	count = props.filterLocation != 'all' ? count + 1 : count;
+	count = props.searchName != '' ? count + 1 : count;  
 	return(
 			<div className='col-xs-12'>
 				<span>Candidate Search Filter</span>
@@ -77,7 +89,7 @@ function Header(props){
 function SelectedFilter(props){
 	return(
 			<div>
-					<span>Showing Candidates from 
+					<span><Button bsStyle='success' bsSize='large'>Get started today</Button>
 					</span>
 			</div>
 		);
@@ -95,7 +107,6 @@ class App extends React.Component{
 		this.handleFilterChange = this.handleFilterChange.bind(this);
 	}
 	handleFilterChange(event){
-		debugger
 		const target = event.target;
 		const name = target.name;
 		const value = target.value;
@@ -114,30 +125,38 @@ class App extends React.Component{
 			companyList.push(candidate.current_company);
 			locationList.push(candidate.current_location);
 			if((filterCompany === 'all' || filterCompany === candidate.current_company) && (filterLocation === 'all' || filterLocation === candidate.current_location) && (searchName === '' || (candidate.first_name+candidate.last_name).toLowerCase().indexOf(searchName.toLowerCase()) > -1 )) {
-				return(<div><Candidate key={candidate.uid} candidate={candidate}/></div>)
+				return(<div className="candidate"><Candidate key={candidate.uid} candidate={candidate}/></div>)
 			}			
 		});
 		return(
-			<div>
+			<Grid>
 				<div>
-					<Header filterCompany={filterCompany} filterLocation={filterLocation} searchName={searchName}/> 				
+					<Row>
+					<Col xs={12}>
+						<Col md={8} sm={12}><Header filterCompany={filterCompany} filterLocation={filterLocation} searchName={searchName}/> </Col>
+						<Col md={4} sm={12}><SearchName whenChanged={this.handleFilterChange}/></Col>	
+					</Col>
 					<CompanyFilter companyList={companyList} whenChanged={this.handleFilterChange}/>	
 					<LocationFilter locationList={locationList} whenChanged={this.handleFilterChange}/>		
-					<SearchName whenChanged={this.handleFilterChange}/>		
+					
+					</Row>	
 				</div>			
-				<div>{candidateList}</div>
-			</div>
+				<div className="container">{candidateList}</div>
+			</Grid>
 			);
 	}
 }
+
+
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://104.199.147.85/candidates', true);
 xhr.responseType = 'json';
 xhr.onload = function(e) {
-  if (this.status == 200) {  	
+  if (this.status == 200) {
   	debugger
   	ReactDOM.render(
-  		<App candidates = {this.response.candidates}/>,  		
+  		<App candidates={this.response.candidates}/>,
   		document.getElementById('root')
   	);
   }
